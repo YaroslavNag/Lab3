17eb31e9e46e3dd012de1f8f24c33a45c76c0ae3
429283-18 <429283-18>
2022-03-17 15:21:55 +0300

    FirstCommit

---
+++ WebApplication1/Models/ContactsContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ADCLG1;Database=YarNag4Lab;Trusted_Connection=True;");
+        }
+    }
+
+}
